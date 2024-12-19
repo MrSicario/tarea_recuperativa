10c735f4255aee21ff9a6f6ba2385129a823bef5
Manuel Saavedra <manuelsaavedrasoto@gmail.com>
2024-12-19 15:47:40 -0300

    basic structure

---
+++ src/cpp/main.cpp
@@ -0,0 +1,16 @@
+#include "lib/PerfectHash.hpp"
+#include "lib/PrimeGen.hpp"
+
+// Experiment 1
+void exp1() {}
+
+// Experiment 2
+void exp2() {}
+
+// Experiment 3
+void exp3() {}
+
+// Experiment 4
+void exp4() {}
+
+int main() {}
